5c0e3a30ff75ea0e120f2fe0d46fa78002739251
Stephen Hosom <0xhosom@gmail.com>
2016-07-29 10:21:23 -0400

    fix select assignment

--- mandrake/mandrake.go
+++ mandrake/mandrake.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"golang.org/x/exp/inotify"
 	"github.com/hosom/gomandrake/config"
-	"github.com/hosom/gomandrake/inputs"
 	//"github.com/hosom/gomandrake/plugin"
 )
 
@@ -22,9 +21,7 @@ func NewMandrake(c config.Config) (*Mandrake, error) {
 
 func (m Mandrake) ListenAndServe() {
 	go m.DispatchAnalysis()
-
-	m.Monitor("/tmp")
-	//plugin.CreateListenerAndServe(m.AnalysisPipeline, m.Input)
+	go m.Monitor()
 }
 
 func (m Mandrake) DispatchAnalysis() {	
@@ -33,13 +30,13 @@ func (m Mandrake) DispatchAnalysis() {
 	}
 }
 
-func (m Mandrake) Monitor(directory string) {
+func (m Mandrake) Monitor() {
 	watcher, err := inotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = watcher.AddWatch(directory, inotify.IN_CLOSE_WRITE)
+	err = watcher.AddWatch(m.MonitoredDirectory, inotify.IN_CLOSE_WRITE)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,7 +45,7 @@ func (m Mandrake) Monitor(directory string) {
 		select {
 		case ev := <- watcher.Event:
 			m.AnalysisPipeline <- ev.Name
-		case err := watcher.Error:
+		case err := <- watcher.Error:
 			log.Printf("inotify error: %s", err)
 		}
 	}
