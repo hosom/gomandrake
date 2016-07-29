dbbdf6948a4815003eae859f00eb7de0ae60c12e
Stephen Hosom <0xhosom@gmail.com>
2016-07-29 10:54:54 -0400

    Simplify NewMandrake and add documentation

--- mandrake/mandrake.go
+++ mandrake/mandrake.go
@@ -5,32 +5,43 @@ package mandrake
 
 import (
 	"log"
+
 	"golang.org/x/exp/inotify"
 	"github.com/hosom/gomandrake/config"
 	//"github.com/hosom/gomandrake/plugin"
 )
 
+// Mandrake is a wrapper struct for the bulk of the application logic
 type Mandrake struct {
 	AnalysisPipeline	chan string
 	MonitoredDirectory	string
 }
 
-func NewMandrake(c config.Config) (*Mandrake, error) {
-	return &Mandrake{make(chan string), c.MonitoredDirectory}, nil
+// NewMandrake creates and returns a Mandrake struct utilizing a passed 
+// parsed configuration file to create the correct fields.
+func NewMandrake(c config.Config) Mandrake {
+	return Mandrake{make(chan string), c.MonitoredDirectory}
 }
 
+// ListenAndServe starts the goroutines that perform all of the heavy lifting
+// including Monitor() and DispatchAnalysis(). 
 func (m Mandrake) ListenAndServe() {
 	log.SetPrefix("[mandrake] ")
 	go m.DispatchAnalysis()
 	m.Monitor()
 }
 
+// DispatchAnalysis intelligently sends a new file to registered plugins so
+// that it can be analyzed.
 func (m Mandrake) DispatchAnalysis() {	
 	for fpath := range m.AnalysisPipeline {
 		log.Printf("%s", fpath)
 	}
 }
 
+// Monitor uses inotify to monitor the MonitoredDirectory for IN_CLOSE_WRITE
+// events. Files written to the MonitoredDirectory will be sent to the 
+// analysis pipeline to be analyzed.
 func (m Mandrake) Monitor() {
 	log.Println("starting inotify watcher")
 	watcher, err := inotify.NewWatcher()
