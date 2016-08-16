43f424c6532d4440fd999ec46cd231ba985eb54d
Stephen Hosom <0xhosom@gmail.com>
2016-08-16 15:00:49 -0400

    update example plugin

--- example/HelloWorld.go
+++ example/HelloWorld.go
@@ -4,23 +4,27 @@ package main
 import (
 	"fmt"
 	"log"
+	"encoding/json"
 	"net/rpc/jsonrpc"
 
+	"github.com/hosom/gomandrake/filemeta"
 	"github.com/natefinch/pie"
 )
 
 type HelloWorld struct {}
 
-func (HelloWorld) Analyze(fname string, response *string) error {
-	log.Printf("Received call for Hello with name: %q", fname)
+func (HelloWorld) Analyze(fmeta string, response *string) error {
+	var fm filemeta.FileMeta
+	json.Unmarshal([]byte(fmeta), &fm)
+	log.Printf("Received call for Hello with name: %q", fm.Filepath)
 
-	*response = fmt.Sprintf("{\"Hello\":%q}", fname)
+	*response = fmt.Sprintf("{\"Hello\":%q}", fm.Filepath)
 
 	return nil
 }
 
 func main () {
-	log.SetPrefix("[helloworld ] ")
+	log.SetPrefix("[helloworld]")
 	p := pie.NewProvider()
 
 	if err := p.RegisterName("HelloWorld", HelloWorld{}); err != nil {
