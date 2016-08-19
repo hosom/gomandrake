46a3f10edece343d5e23e05a301bc3c9e102f8e2
Stephen Hosom <0xhosom@gmail.com>
2016-08-19 11:27:09 -0400

    removed python module from examples

--- example/JSONLogger.go
+++ example/JSONLogger.go
@@ -1,3 +1,7 @@
+/*
+JSONLogger is a plugin for mandrake that provides basic logging services to 
+log records as JSON objects. One record is logged on each line.
+*/
 package main
 
 import (
@@ -11,10 +15,14 @@ import (
 	"github.com/natefinch/pie"
 )
 
+// JSONLogger is the API struct that describes the exposed API.
 type JSONLogger struct {
+	// LogFile is the open file that is being written to. 
 	LogFile			*os.File
 }
 
+// Log exposes the API for the main mandrake daemon to send requests for
+// analysis records to be logged. 
 func (j JSONLogger) Log(record string, response *string) error {
 	_, err := j.LogFile.WriteString(fmt.Sprintf("%s\n", record))
 	if err != nil {
@@ -25,6 +33,8 @@ func (j JSONLogger) Log(record string, response *string) error {
 	return nil
 }
 
+// main is the primary body of code for the plugin. Here command line
+// arguments are parsed, the API is established and served.
 func main() {
 	log_path := flag.String("output", filepath.FromSlash("/var/log/mandrake.log"),
 							"location for the plugin to send the log")
@@ -39,6 +49,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer f.Close()
 
 	p := pie.NewProvider()
 
